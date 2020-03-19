b8a64d989464fff4903317bdc0e28df89583fce6
Yeseul Jang <jangysseul@gmail.com>
2020-03-19 18:48:31 -0400

    add validation for student registration

--- src/components/CreateStudent.js
+++ src/components/CreateStudent.js
@@ -19,10 +19,11 @@ function CreateStudent(props) {
       phoneNumber:'',
       email:'',
       program:''
-
+    
     });
   const [showLoading, setShowLoading] = useState(false);
   const apiUrl = "http://localhost:3000/";
+  const [validated, setValidated] = useState(false);
 
   const saveUser = (e) => {
     setShowLoading(true);
@@ -37,8 +38,16 @@ function CreateStudent(props) {
         phoneNumber: student.phoneNumber,
         email: student.email,
         program:student.program
+        
     };
-    
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
+
     axios.post(apiUrl, data)
       .then((result) => {
         setShowLoading(false);
@@ -59,52 +68,79 @@ function CreateStudent(props) {
         </Spinner> 
       } 
       <Jumbotron>
-        <Form onSubmit={saveUser}>
+        <Form noValidate validated={validated} onSubmit={saveUser}>
 
-          <Form.Group>
+          <Form.Group  >
             <Form.Label> Student Number </Form.Label>
-            <Form.Control type="number" name="studentNumber" id="studentNumber" placeholder="Enter Student Number" value={student.studentNumber} onChange={onChange} />
+            <Form.Control required type="number" name="studentNumber" id="studentNumber" placeholder="Enter Student Number" value={student.studentNumber} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+              Please provide student number.
+            </Form.Control.Feedback>
           </Form.Group>
        
-          <Form.Group>
+          <Form.Group >
             <Form.Label> Password </Form.Label>
-            <Form.Control type="text" name="password" id="password" placeholder="Enter the password" value={student.password} onChange={onChange} />
+            <Form.Control required type="text" name="password" id="password" placeholder="Enter the password" value={student.password} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+              Please provide password.
+            </Form.Control.Feedback>
           </Form.Group>
        
        
           <Form.Group>
             <Form.Label> First Name</Form.Label>
-            <Form.Control type="text" name="firstName" id="firstName" placeholder="Enter first name" value={student.firstName} onChange={onChange} />
+            <Form.Control required type="text" name="firstName" id="firstName" placeholder="Enter first name" value={student.firstName} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+              Please provide first name.
+            </Form.Control.Feedback>
           </Form.Group>
        
           <Form.Group>
             <Form.Label> Last Name </Form.Label>
-            <Form.Control type="text" name="lastName" id="lastName" placeholder="Enter last name" value={student.lastName} onChange={onChange} />
+            <Form.Control required type="text" name="lastName" id="lastName" placeholder="Enter last name" value={student.lastName} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+              Please provide last name.
+            </Form.Control.Feedback>
           </Form.Group>
        
           <Form.Group>
             <Form.Label> Address </Form.Label>
-            <Form.Control type="text" name="address" id="address" placeholder="Enter the address" value={student.address} onChange={onChange} />
+            <Form.Control required type="text" name="address" id="address" placeholder="Enter the address" value={student.address} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+             Please fill a valid email addres.
+            </Form.Control.Feedback>
           </Form.Group>
        
           <Form.Group>
             <Form.Label> City </Form.Label>
-            <Form.Control type="text" name="city" id="city" placeholder="Enter the city" value={student.city} onChange={onChange} />
+            <Form.Control required type="text" name="city" id="city" placeholder="Enter the city" value={student.city} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+              Please provide city.
+            </Form.Control.Feedback>
           </Form.Group>
        
           <Form.Group>
             <Form.Label> Phone Number </Form.Label>
-            <Form.Control type="text" name="phoneNumber" id="phoneNumber" placeholder="Enter the phone number" value={student.phoneNumber} onChange={onChange} />
+            <Form.Control required type="number" name="phoneNumber" id="phoneNumber" placeholder="Enter the phone number" value={student.phoneNumber} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+              Please provide phone number.
+            </Form.Control.Feedback>
           </Form.Group>
        
           <Form.Group>
             <Form.Label>Email</Form.Label>
-            <Form.Control type="text" name="email" id="email" rows="3" placeholder="Enter email" value={student.email} onChange={onChange} />
+            <Form.Control required type="text" name="email" id="email" rows="3" placeholder="Enter email" value={student.email} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+              Please provide email addreess.
+            </Form.Control.Feedback>
           </Form.Group>
        
           <Form.Group>
             <Form.Label> Program </Form.Label>
-            <Form.Control type="text" name="program" id="program" placeholder="Enter the program" value={student.program} onChange={onChange} />
+            <Form.Control required type="text" name="program" id="program" placeholder="Enter the program" value={student.program} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+              Please provide program.
+            </Form.Control.Feedback>
           </Form.Group>
        
           <Button variant="primary" type="submit">
