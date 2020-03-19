ca77ba6a7ef402fbdbd313e88a6864b2ad51d387
KyungjinJeong <57504664+KyungjinJeong@users.noreply.github.com>
2020-03-19 19:54:40 -0400

    permission to edit/delete course

--- src/components/EditCourse.js
+++ src/components/EditCourse.js
@@ -5,27 +5,31 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { withRouter } from 'react-router-dom';
+import Alert from 'react-bootstrap/Alert';
 
 function EditCourse(props) {
   const [course, setCourse] = useState(
-    { _id: '',
-     courseCode: '',
-     courseName: '',
-     section: '',
-     semester: '',
-    studentNumber:'' }); 
+    {
+      _id: '',
+      courseCode: '',
+      courseName: '',
+      section: '',
+      semester: '',
+      studentNumber: ''
+    });
 
   const [showLoading, setShowLoading] = useState(true);
   const apiUrl = "http://localhost:3000/api/courses/" + props.match.params.id;
   const [validated, setValidated] = useState(false);
+
   //runs only once after the first render
   useEffect(() => {
     setShowLoading(false);
     //call api
     const fetchData = async () => {
       const result = await axios(apiUrl);
-      setCourse(result.data);
-      console.log(result.data);
+      setCourse(result.data.course);
+      console.log(result.data.course);
       setShowLoading(false);
     };
 
@@ -36,11 +40,11 @@ function EditCourse(props) {
     setShowLoading(true);
     e.preventDefault();
     const data = {
-        courseCode: course.courseCode,
-        courseName: course.courseName,
-        section: course.section,
-        semester: course.semester,
-        studentNumber: course.studentNumber
+      courseCode: course.courseCode,
+      courseName: course.courseName,
+      section: course.section,
+      semester: course.semester,
+      studentNumber: course.studentNumber
     };
     const form = e.currentTarget;
     if (form.checkValidity() === false) {
@@ -51,7 +55,7 @@ function EditCourse(props) {
     setValidated(true);
     axios.put(apiUrl, data)
       .then((result) => {
-        console.log('after calling put to update',result.data )
+        console.log('after calling put to update', result.data);
         setShowLoading(false);
         props.history.push('/showCourse/' + props.match.params.id)
       }).catch((error) => setShowLoading(false));
@@ -59,55 +63,56 @@ function EditCourse(props) {
   //runs when user enters a field
   const onChange = (e) => {
     e.persist();
-    setCourse({...course, [e.target.name]: e.target.value});
+    setCourse({ ...course, [e.target.name]: e.target.value });
   }
 
   return (
     <div>
-      {showLoading && 
+      {showLoading &&
         <Spinner animation="border" role="status">
           <span className="sr-only">Loading...</span>
-        </Spinner> 
-      } 
+        </Spinner>
+      }
+
       <Jumbotron>
-      <Form noValidate validated={validated} onSubmit={updateCourse}>
-              <Form.Group>
-                <Form.Label> Course Code</Form.Label>
-                <Form.Control  required type="text" name="courseCode" id="courseCode" placeholder="Enter Course Code" value={course.courseCode} onChange={onChange} />
-                <Form.Control.Feedback type="invalid">
+        <Form noValidate validated={validated} onSubmit={updateCourse}>
+          <Form.Group>
+            <Form.Label> Course Code</Form.Label>
+            <Form.Control required type="text" name="courseCode" id="courseCode" placeholder="Enter Course Code" value={course.courseCode} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
               Please provide correct course code.
             </Form.Control.Feedback>
-              </Form.Group>
+          </Form.Group>
 
-              <Form.Group>
-                <Form.Label> Course Name</Form.Label>
-                <Form.Control required type="text" name="courseName" id="courseName" placeholder="Enter Course Name" value={course.courseName} onChange={onChange} />
-                <Form.Control.Feedback type="invalid">
-                Please provide course name.
+          <Form.Group>
+            <Form.Label> Course Name</Form.Label>
+            <Form.Control required type="text" name="courseName" id="courseName" placeholder="Enter Course Name" value={course.courseName} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+              Please provide course name.
                 </Form.Control.Feedback>
-              </Form.Group>
+          </Form.Group>
 
-              <Form.Group>
-                <Form.Label> Section</Form.Label>
-                <Form.Control required type="text" name="section" id="section" placeholder="Enter section" value={course.section} onChange={onChange} />
-                <Form.Control.Feedback type="invalid">
-                Please provide section.
+          <Form.Group>
+            <Form.Label> Section</Form.Label>
+            <Form.Control required type="text" name="section" id="section" placeholder="Enter section" value={course.section} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+              Please provide section.
                 </Form.Control.Feedback>
-              </Form.Group>
+          </Form.Group>
 
-              <Form.Group>
-                <Form.Label> Semester</Form.Label>
-                <Form.Control required  type="text" name="semester" id="semester" placeholder="Enter semester" value={course.semester} onChange={onChange} />
-                <Form.Control.Feedback type="invalid">
-                Please provide semester.
+          <Form.Group>
+            <Form.Label> Semester</Form.Label>
+            <Form.Control required type="text" name="semester" id="semester" placeholder="Enter semester" value={course.semester} onChange={onChange} />
+            <Form.Control.Feedback type="invalid">
+              Please provide semester.
                 </Form.Control.Feedback>
-              </Form.Group>
-                            
-              <Button variant="primary" type="submit">
-                Update Course
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Update Course
               </Button>
-            </Form>
-        
+        </Form>
+
       </Jumbotron>
     </div>
   );
--- src/components/ShowCourse.js
+++ src/components/ShowCourse.js
@@ -5,6 +5,7 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import { withRouter } from 'react-router-dom';
 import Table from 'react-bootstrap/Table';
+import Alert from 'react-bootstrap/Alert';
 
 function ShowCourse(props) {
   const [data, setData] = useState({});
@@ -12,13 +13,16 @@ function ShowCourse(props) {
   const apiUrl = "http://localhost:3000/api/courses/" + props.match.params.id;
   const apiStudentListUrl = "http://localhost:3000/api/studentListByCourse/" + props.match.params.id;
   const [studentData, setStudentData] = useState([]);
+  const [sessionId, setSessionId] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     setShowLoading(false);
     const fetchData = async () => {
       const result = await axios(apiUrl);
       console.log('results from courses', result.data);
-      setData(result.data);
+      setData(result.data.course);
+      setSessionId(result.data.sessionId);
 
       const resultStudentList = await axios(apiStudentListUrl);
       console.log('results from studentList', resultStudentList.data);
@@ -30,27 +34,43 @@ function ShowCourse(props) {
     fetchData();
   }, []);
 
-  const editCourse = (id) => {
-    props.history.push({
-      pathname: '/editCourse/' + id
-    });
+  const editCourse = (id, creatorId) => {
+    console.log("creatorId >>> ", creatorId);
+
+    if (creatorId != sessionId) {
+      console.log("diff session !!");
+      setMessage('You are not allow to edit course !');
+
+    } else {
+      props.history.push({
+        pathname: '/editCourse/' + id
+      });
+    }
 
   };
 
-  const deleteCourse = (id) => {
-    setShowLoading(true);
-    const course = {
-      courseCode: data.courseCode,
-      courseName: data.courseName,
-      section: data.section,
-      semester: data.semester,
-      studentNumber: data.studentNumber
-    };
-    axios.delete(apiUrl, course)
-      .then((result) => {
-        setShowLoading(false);
-        props.history.push('/listCourses')
-      }).catch((error) => setShowLoading(false));
+  const deleteCourse = (id, creatorId) => {
+    console.log("creatorId >>> ", creatorId);
+
+    if (creatorId != sessionId) {
+      console.log("diff session !!");
+      setMessage('You are not allow to delete course !');
+
+    } else {
+      setShowLoading(true);
+      const course = {
+        courseCode: data.courseCode,
+        courseName: data.courseName,
+        section: data.section,
+        semester: data.semester,
+        studentNumber: data.studentNumber
+      };
+      axios.delete(apiUrl, course)
+        .then((result) => {
+          setShowLoading(false);
+          props.history.push('/listCourses')
+        }).catch((error) => setShowLoading(false));
+    }
   };
 
   return (
@@ -58,6 +78,13 @@ function ShowCourse(props) {
       {showLoading && <Spinner animation="border" role="status">
         <span className="sr-only">Loading...</span>
       </Spinner>}
+
+      {message != '' &&
+        <Alert variant="danger">
+          Message : {message}
+        </Alert>
+      }
+
       <Jumbotron>
         <h1>Course Name: {data.courseName}</h1>
         <p>Course Code: {data.courseCode}</p>
@@ -65,8 +92,8 @@ function ShowCourse(props) {
         <p>Semester: {data.semester}</p>
 
         <p>
-          <Button type="button" variant="primary" onClick={() => { editCourse(data._id) }}>Edit</Button>&nbsp;
-          <Button type="button" variant="danger" onClick={() => { deleteCourse(data._id) }}>Delete</Button>
+          <Button type="button" variant="primary" onClick={() => { editCourse(data._id, data.creator) }}>Edit</Button>&nbsp;
+          <Button type="button" variant="danger" onClick={() => { deleteCourse(data._id, data.creator) }}>Delete</Button>
         </p>
       </Jumbotron>
 
